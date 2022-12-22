cdde684a17df0648118a6a3963a41dd3db1100fb
Dev Banda <56268243+WalterBanda@users.noreply.github.com>
2022-12-22 09:21:38 +0000

    ☕ Updated server URL to deployed one

--- server.js
+++ server.js
@@ -9,178 +9,179 @@ app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/login", function (req, res) {
+app.get("/login", function(req, res) {
+  client.get('https://airport-flask-backend.walterbanda.repl.co')
   res.render("login", { invalid: undefined });
 });
 
-app.post("/login", function (req, res) {
+app.post("/login", function(req, res) {
   client
-    .post("http://127.0.0.1:5000/login", {
+    .post("https://airport-flask-backend.walterbanda.repl.co/login", {
       username: req.body.username,
       password: req.body.password,
     })
-    .then(function (response) {
+    .then(function(response) {
       res.redirect("/");
     })
-    .catch(function (error) {
+    .catch(function(error) {
       res.render("login", {
         invalid: `${error.response.data?.message ?? "Error Occurred"}`,
       });
     });
 });
 
-app.get("/airport", function (req, res) {
+app.get("/airport", function(req, res) {
   client
-    .get("http://127.0.0.1:5000/airports")
-    .then(function (response) {
+    .get("https://airport-flask-backend.walterbanda.repl.co/airports")
+    .then(function(response) {
       res.render("airport", { airports: response.data });
     })
-    .catch(function (error) {
+    .catch(function(error) {
       res.render("airport", { airports: [] });
     });
 });
 
-app.post("/airport", function (req, res) {
+app.post("/airport", function(req, res) {
   client
-    .post("http://127.0.0.1:5000/airports", {
+    .post("https://airport-flask-backend.walterbanda.repl.co/airports", {
       airportcode: req.body.airportcode,
       airportname: req.body.airportname,
       country: req.body.country,
     })
-    .then(function (response) {
+    .then(function(response) {
       res.redirect("airport");
     })
-    .catch(function (error) {
+    .catch(function(error) {
       res.redirect("airport");
     });
 });
 
-app.get("/airport/id/:slug", function (req, res) {
+app.get("/airport/id/:slug", function(req, res) {
   client
-    .get(`http://127.0.0.1:5000/airport/${req.params.slug}`)
-    .then(function (response) {
+    .get(`https://airport-flask-backend.walterbanda.repl.co/airport/${req.params.slug}`)
+    .then(function(response) {
       res.render("airport-id", { airport: response.data });
     })
-    .catch(function (error) {
+    .catch(function(error) {
       res.redirect("/");
     });
 });
 
-app.post("/airport/update/:slug", function (req, res) {
+app.post("/airport/update/:slug", function(req, res) {
   console.log(req.body, req.params.slug);
   client
-    .put(`http://127.0.0.1:5000/airport/${req.params.slug}`, {
+    .put(`https://airport-flask-backend.walterbanda.repl.co/airport/${req.params.slug}`, {
       airportcode: req.body.airportcode,
       airportname: req.body.airportname,
       country: req.body.country,
     })
-    .then(function (response) {
+    .then(function(response) {
       res.redirect("/airport");
     })
-    .catch(function (error) {
+    .catch(function(error) {
       res.redirect("/airport");
     });
 });
 
-app.post("/airport/delete/:slug", function (req, res) {
+app.post("/airport/delete/:slug", function(req, res) {
   client
-    .delete(`http://127.0.0.1:5000/airport/${req.params.slug}`)
-    .then(function (response) {
+    .delete(`https://airport-flask-backend.walterbanda.repl.co/airport/${req.params.slug}`)
+    .then(function(response) {
       res.redirect("/airport");
     })
-    .catch(function (error) {
+    .catch(function(error) {
       res.redirect("/airport");
     });
 });
 
-app.get("/plane", function (req, res) {
+app.get("/plane", function(req, res) {
   client
-    .get("http://127.0.0.1:5000/planes")
-    .then(function (response) {
+    .get("https://airport-flask-backend.walterbanda.repl.co/planes")
+    .then(function(response) {
       res.render("plane", { planes: response.data });
     })
-    .catch(function (error) {
+    .catch(function(error) {
       res.render("plane", { planes: [] });
     });
 });
 
-app.post("/plane", function (req, res) {
+app.post("/plane", function(req, res) {
   client
-    .post("http://127.0.0.1:5000/planes", {
+    .post("https://airport-flask-backend.walterbanda.repl.co/planes", {
       make: req.body.make,
       model: req.body.model,
       year: req.body.year,
       capacity: req.body.capacity,
     })
-    .then(function (response) {
+    .then(function(response) {
       res.redirect("/plane");
     })
-    .catch(function (error) {
+    .catch(function(error) {
       res.redirect("/plane");
     });
 });
 
-app.get("/plane/id/:slug", function (req, res) {
+app.get("/plane/id/:slug", function(req, res) {
   client
-    .get(`http://127.0.0.1:5000/plane/${req.params.slug}`)
-    .then(function (response) {
+    .get(`https://airport-flask-backend.walterbanda.repl.co/plane/${req.params.slug}`)
+    .then(function(response) {
       res.render("plane-id", { plane: response.data });
     })
-    .catch(function (error) {
+    .catch(function(error) {
       res.redirect("/plane");
     });
 });
 
-app.post("/plane/update/:slug", function (req, res) {
+app.post("/plane/update/:slug", function(req, res) {
   console.log(req.body, req.params.slug);
   client
-    .put(`http://127.0.0.1:5000/plane/${req.params.slug}`, {
+    .put(`https://airport-flask-backend.walterbanda.repl.co/plane/${req.params.slug}`, {
       make: req.body.make,
       model: req.body.model,
       year: req.body.year,
       capacity: req.body.capacity,
     })
-    .then(function (response) {
+    .then(function(response) {
       res.redirect("/plane");
     })
-    .catch(function (error) {
+    .catch(function(error) {
       res.redirect("/plane");
     });
 });
 
-app.post("/plane/delete/:slug", function (req, res) {
+app.post("/plane/delete/:slug", function(req, res) {
   client
-    .delete(`http://127.0.0.1:5000/plane/${req.params.slug}`)
-    .then(function (response) {
+    .delete(`https://airport-flask-backend.walterbanda.repl.co/plane/${req.params.slug}`)
+    .then(function(response) {
       res.redirect("/plane");
     })
-    .catch(function (error) {
+    .catch(function(error) {
       res.redirect("/plane");
     });
 });
 
-app.post("/flight", function (req, res) {
+app.post("/flight", function(req, res) {
   console.log(req.body);
   client
-    .post("http://127.0.0.1:5000/flights", {
+    .post("https://airport-flask-backend.walterbanda.repl.co/flights", {
       plane_id: req.body.plane_id,
       airportfrom_id: req.body.airportfrom_id,
       airportto_id: req.body.airportto_id,
       date: req.body.date,
     })
-    .then(function (response) {
+    .then(function(response) {
       res.redirect("/flight");
     })
-    .catch(function (error) {
+    .catch(function(error) {
       res.redirect("/flight");
     });
 });
 
-app.get("/flight", async function (req, res) {
+app.get("/flight", async function(req, res) {
   try {
-    const getPlanes = await client.get("http://127.0.0.1:5000/planes");
-    const getAirports = await client.get("http://127.0.0.1:5000/airports");
-    const getFlights = await client.get("http://127.0.0.1:5000/flights");
+    const getPlanes = await client.get("https://airport-flask-backend.walterbanda.repl.co/planes");
+    const getAirports = await client.get("https://airport-flask-backend.walterbanda.repl.co/airports");
+    const getFlights = await client.get("https://airport-flask-backend.walterbanda.repl.co/flights");
 
     res.render("flight", {
       flights: getFlights.data,
@@ -192,21 +193,21 @@ app.get("/flight", async function (req, res) {
   }
 });
 
-app.post("/flight/delete/:slug", function (req, res) {
+app.post("/flight/delete/:slug", function(req, res) {
   client
-    .delete(`http://127.0.0.1:5000/flight/${req.params.slug}`)
-    .then(function (response) {
+    .delete(`https://airport-flask-backend.walterbanda.repl.co/flight/${req.params.slug}`)
+    .then(function(response) {
       res.redirect("/flight");
     })
-    .catch(function (error) {
+    .catch(function(error) {
       res.redirect("/flight");
     });
 });
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.redirect("airport");
 });
 
-app.listen(3000, function () {
+app.listen(3000, function() {
   console.log("Server Started 🚀");
 });
